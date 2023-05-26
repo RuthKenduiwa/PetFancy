9829f5e7e538a6b59735e872c9f05fc569569f6c
ruthkenduiwa <ruthkenduiwa@gmail.com>
2023-05-26 12:08:16 +0300

    added new changes on responsiveness

--- js/main.js
+++ js/main.js
@@ -4,29 +4,27 @@ document.querySelector("#login-btn").onclick = () =>{
     login.classList.toggle('active'); 
 }
 
-let navbar = document.querySelector(".header .navbar");
-
-document.querySelector('#menu-btn').onclick = () =>{
-    login.classList.remove('active');
-    navbar.classList.toggle('active');
+let navbar = document.querySelector(".navbar");
+document.querySelector("#menu-btn").onclick = () =>{
+    navbar.classList.toggle('active'); 
 }
 
-var swiper = new Swiper(".gallery-slider",{
-    grabCursor:true,
-    loop:true,
-    centeredSlides:true,
-    spaceBetween:20,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        nextEl: ".swiper-button-next",
-    },
-    breakpoints: {
-        0:{
-            slidesPerView:1,
-        },
-        700:{
-            slidesPerView:2,
-        }
-    }
-}
-)
+// var swiper = new Swiper(".gallery-slider",{
+//     grabCursor:true,
+//     loop:true,
+//     centeredSlides:true,
+//     spaceBetween:20,
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         nextEl: ".swiper-button-next",
+//     },
+//     breakpoints: {
+//         0:{
+//             slidesPerView:1,
+//         },
+//         700:{
+//             slidesPerView:2,
+//         }
+//     }
+// }
+// )
