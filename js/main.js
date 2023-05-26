c473a2f429b49b54d0fc0f06c7cb34ba60be0bbd
ruthkenduiwa <ruthkenduiwa@gmail.com>
2023-05-26 09:06:02 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let login = document.querySelector(".login-form");
+
+document.querySelector("#login-btn").onclick = () =>{
+    login.classList.toggle('active'); 
+}
+
+let navbar = document.querySelector(".header .navbar");
+
+document.querySelector('#menu-btn').onclick = () =>{
+    login.classList.remove('active');
+    navbar.classList.toggle('active');
+}
+
+var swiper = new Swiper(".gallery-slider",{
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    spaceBetween:20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        nextEl: ".swiper-button-next",
+    },
+    breakpoints: {
+        0:{
+            slidesPerView:1,
+        },
+        700:{
+            slidesPerView:2,
+        }
+    }
+}
+)
